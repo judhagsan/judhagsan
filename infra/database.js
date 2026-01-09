3473b500093585ad672d56c91de289afcbb67c02
Judhá Santos <judhagsan@gmail.com>
2026-01-08 23:44:26 -0300

    feat: add new files for new website

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,20 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
-async function query(queryObeject) {
+async function query(queryObject) {
   let client;
   try {
     client = await getNewClient();
-    const result = await client.query(queryObeject);
+    const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com Banco ou na Query.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
@@ -23,6 +27,7 @@ async function getNewClient() {
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
+
   await client.connect();
   return client;
 }
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -12,6 +12,7 @@ describe("GET /api/v1/migrations", () => {
       expect(response.status).toBe(200);
 
       const responseBody = await response.json();
+
       expect(Array.isArray(responseBody)).toBe(true);
       expect(responseBody.length).toBeGreaterThan(0);
     });
