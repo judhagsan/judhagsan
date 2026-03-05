1e392c81605fada3cc910bf03c8657285317f40a
Judhagsan <judhagsan@gmail.com>
2026-03-05 18:26:46 -0300

    feat: apply `authorization.filterOutpu()` to all endpoints

--- tests/integration/infra/email.test.js
+++ tests/integration/infra/email.test.js
@@ -10,22 +10,22 @@ describe("infra/email.js", () => {
     await orchestrator.deleteAllEmails();
 
     await email.send({
-      from: "FinTab <contato@fintab.com.br>",
-      to: "contato@curso.dev",
+      from: "Judhagsan <judhagsan@gmail.com>",
+      to: "judhagsan@gmail.com",
       subject: "Teste de assunto",
       text: "Teste de corpo.",
     });
 
     await email.send({
-      from: "FinTab <contato@fintab.com.br>",
-      to: "contato@curso.dev",
+      from: "Judhagsan <judhagsan@gmail.com>",
+      to: "judhagsan@gmail.com",
       subject: "Último email enviado",
       text: "Corpo do último email.",
     });
 
     const lastEmail = await orchestrator.getLastEmail();
-    expect(lastEmail.sender).toBe("<contato@fintab.com.br>");
-    expect(lastEmail.recipients[0]).toBe("<contato@curso.dev>");
+    expect(lastEmail.sender).toBe("<judhagsan@gmail.com>");
+    expect(lastEmail.recipients[0]).toBe("<judhagsan@gmail.com>");
     expect(lastEmail.subject).toBe("Último email enviado");
     expect(lastEmail.text).toBe("Corpo do último email.\r\n");
   });
